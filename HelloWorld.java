6c0a6e16fdd6b82c9fcacdb62488f5977196c9b5
varunsharmatrainer <31035263+varunsharmatrainer@users.noreply.github.com>
2019-08-05 20:27:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
-// Your First Program
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
 }
